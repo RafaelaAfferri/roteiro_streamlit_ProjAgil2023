7951dacd0979087bb94687af6221e6e8ec905496
Rafaela Afférri de Oliveira <rafaelaao@al.insper.edu.br>
2023-10-17 08:50:03 -0300

    roteiro 1 e 2 feitos

--- roteiro_1/app.py
+++ roteiro_1/app.py
@@ -2,5 +2,7 @@ import streamlit as st
 
 st.title("Roteiro 1: Primeiros Passos com Streamlit")
 
+st.text_input("Digite algo", key="input_text")
+
 if st.button("Clique-me"):
-    st.write("Você clicou no botão!")
+    st.write(st.session_state.input_text)
--- roteiro_2/app.py
+++ roteiro_2/app.py
@@ -1,7 +1,24 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Roteiro 2: Widgets Interativos")
 
 value = st.slider("Escolha um valor", 0, 100)
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox('Escolha um numero',
+     df['first column'])
+
+radio = st.radio('Escolha um numero', df['second column'])
+
+toggle = st.toggle('toglle')
+
+select_box = st.checkbox('selecione')
+
 if st.button("Mostrar valor do slider"):
-    st.write(f"Valor escolhido: {value}")
+    st.write(f"Valor escolhido: {value, option, radio, select_box}")
+
